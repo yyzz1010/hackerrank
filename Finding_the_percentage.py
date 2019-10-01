7655f4ef72e1670627419648f7a452ea3501cd17
Karen Wong <45536868+yyzz1010@users.noreply.github.com>
2019-10-01 16:10:30 +0800

    Create Finding_the_percentage.py

---
+++ Finding_the_percentage.py
@@ -0,0 +1,10 @@
+from statistics import mean
+
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+print('{:.2f}'.format(mean(student_marks[query_name])))
