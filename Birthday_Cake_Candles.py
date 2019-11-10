585d8c9e4440c9c8e0e580fb0a964fc95b699ef9
Karen Wong <yyyzzz10104@gmail.com>
2019-11-10 21:50:41 +0800

    Create Birthday_Cake_Candles.py

---
+++ Birthday_Cake_Candles.py
@@ -0,0 +1,17 @@
+import os
+
+def birthdayCakeCandles(ar):
+    return ar.count(max(ar))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
